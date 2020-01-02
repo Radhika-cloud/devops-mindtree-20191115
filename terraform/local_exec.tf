2861887533e6122037fc6e0b175e6273702f46bd
Radhika-cloud <55882387+Radhika-cloud@users.noreply.github.com>
2020-01-02 09:06:59 +0530

    modified

--- terraform/local_exec.tf
+++ terraform/local_exec.tf
@@ -39,11 +39,9 @@ resource "null_resource" "ansible-main" {
 provisioner "local-exec" {
   command = <<EOT
         sleep 100;
-        > jenkins-ci.ini;
-        echo "[jenkins-ci]"| tee -a jenkins-ci.ini;
         export ANSIBLE_HOST_KEY_CHECKING=False;
         echo "${aws_instance.backend.public_ip}" | tee -a jenkins-ci.ini;
-        ansible-playbook -e  sshKey=${var.pvt_key} -i jenkins-ci.ini ./ansible/setup-backend.yaml -u ubuntu -v
+        ansible-playbook -e  sshKey=${var.pvt_key} ./ansible/setup-backend.yaml -u ubuntu -v
     EOT
 }
 }
